fa393a7ce7e2c36f45b033303a0088c042902a7f
sreeragp07 <84373517+sreeragp07@users.noreply.github.com>
2022-09-05 10:25:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+//var num=Math.floor(Math.random()*4)+1;
+
+$('#card').wScratchPad({
+    size        : 10,          // The size of the brush/scratch.
+    bg          : 'images/sam3.jpg',  // Background (image path or hex color).
+    fg          :  'images/sam1.jpg',  // Foreground (image path or hex color).
+   
+    cursor      : 'pointer' // Set cursor.
+  });
